8564825342b0de4e180f81550f971f8f1dbd6178
HeadlessHamsterr <joost_rietdijk@live.nl>
2020-02-28 18:18:29 +0100

    test

--- Delta bot/src/main.cpp
+++ Delta bot/src/main.cpp
@@ -1,49 +1,8 @@
-//Max 180 graden
-//Min 85 graden
-
 #include <Arduino.h>
 #include <Servo.h>
 #include "mydef.h"
 
-//#define test
-
-float polsAX = 0;
-float polsAY = 0;
-float polsBX = 0;
-float polsBY = 0;
-float polsCX = 0;
-float polsCY = 0;
-float polsZ = 0;
-
-float elbowAX = 0;
-float elbowAY = 0;
-float elbowAZ = 0;
-float elbowBX = 0;
-float elbowBY = 0;
-float elbowBZ = 0;
-float elbowCX = 0;
-float elbowCY = 0;
-float elbowCZ = 0;
-
-//Struct om de graden voor de servo's op te slaan
-struct servos{
-  int a;
-  int b;
-  int c;
-};
-
-servos graden;
-servos vorigeGradenMax = {85, 85, 85}; //Hoogste aantal graden van de laatste waardes
-servos vorigeGradenMin = {180, 180, 180}; //Laagste aantal graden van de laatste waardes
-
-//Struct om de naar radialen omgerekende graden op te slaan
-struct Radialen{
-  float a;
-  float b;
-  float c;
-};
-
-Radialen radialen;
+#define test
 
 void polsCalc(int x, int y, int z);
 void elbowACalc(int gradenA);
@@ -72,8 +31,14 @@ void setup(){
 
   #ifdef test
     debug();
+    servoA.write(testGraden);
+    servoB.write(testGraden);
+    servoC.write(testGraden);
   #endif
-  move(50, 50, 200);
+  #ifndef test
+    //move(-50, -50, 100);
+    move(9, 7, -134);
+  #endif
 }
 
 void loop(){
@@ -84,7 +49,7 @@ void polsCalc(int x, int y, int z){
   polsAX = x + L_TOP;
   polsAY = y;
 
-  polsZ = z;
+  polsZ = z;//Z_BASIS - z - Z_TOOL_OFFSET;
 
   polsBX = x - (cos(rad60) * L_TOP);
   polsBY = y + (sin(rad60) * L_TOP);
@@ -95,7 +60,7 @@ void polsCalc(int x, int y, int z){
 
 //Functie voor het berekenen van het coördinaat van elleboog A voor een bepaald aantal graden
 void elbowACalc(int gradenA){
-  radialen.a = graden.a * radOmreken;
+  radialen.a = gradenA * radOmreken;
   elbowAX = SERVO_A_X + (L_ARM_1 * cos(radialen.a));
   elbowAY = SERVO_A_Y;
   elbowAZ = SERVO_Z + (L_ARM_1 * sin(radialen.a));
@@ -103,7 +68,7 @@ void elbowACalc(int gradenA){
 
 //Functie voor het berekenen van het coördinaat van elleboog B voor een bepaald aantal graden
 void elbowBCalc(int gradenB){
-  radialen.b = graden.b * radOmreken;
+  radialen.b = gradenB * radOmreken;
   elbowBX = SERVO_B_X - (cos(rad60) * L_ARM_1 * cos(radialen.b));
   elbowBY = SERVO_B_Y + (sin(rad60) * L_ARM_1 * cos(radialen.b));
   elbowBZ = SERVO_Z + (L_ARM_1 * sin(radialen.b));
@@ -111,7 +76,7 @@ void elbowBCalc(int gradenB){
 
 //Functie voor het berekenen van het coördinaat van elleboog C voor een bepaald aantal graden
 void elbowCCalc(int gradenC){
-  radialen.c = graden.c * radOmreken;
+  radialen.c = gradenC * radOmreken;
   elbowCX = SERVO_C_X - (cos(rad60) * L_ARM_1 * cos(radialen.c));
   elbowCY = SERVO_C_Y - (sin(rad60) * L_ARM_1 * cos(radialen.c));
   elbowCZ = SERVO_Z + (L_ARM_1 * sin(radialen.c));
@@ -126,9 +91,11 @@ float distanceCalc(float polsX, float polsY, float elbowX, float elbowY, float p
 //Functie voor het bepalen van de juiste waardes voor de servo's voor een gekozen coördinaat
 void degreeCalc(int x, int y, int z){
   Serial.println("Pols berekenen");
-  graden.a = (MAX + MIN) / 2;  //Begin waardes voor servo's instellen midden tussen de minimale en maximale waardes (85 en 180)
-  graden.b = (MAX + MIN) / 2;
-  graden.c = (MAX + MIN) / 2;
+  gradenMax = {MAX, MAX, MAX};  //Bovengrens instellen als maximale aantal graden
+  gradenMin = {MIN, MIN, MIN};  //Ondergrens instellen als minimale aantal graden
+  graden.a = (gradenMax.a + gradenMin.a) / 2;  //Begin waardes voor servo's instellen midden tussen de minimale en maximale waardes
+  graden.b = (gradenMax.b + gradenMin.b) / 2;
+  graden.c = (gradenMax.c + gradenMin.c) / 2;
   polsCalc(x, y, z);  //Coördinaten van de polsen berekenen
   elbowACalc(graden.a); //Coördinaten van elleboog A berekenen
 
@@ -140,79 +107,85 @@ void degreeCalc(int x, int y, int z){
 
   while (L_ARM_2 - distance < -resolution || L_ARM_2 - distance > resolution){  //Controleren of de berekende afstand van de arm binnen de grenzen valt
     if(L_ARM_2 - distance < -resolution){ //De berekende afstand is groter dan de arm
-      graden.a = ;  //Graden kiezen in het midden tussen 0 en de vorige waarde
+      /*gradenMax.a =  graden.a; //Bovengrens van aantal graden aanpassen naar momentele positie
+      graden.a = (gradenMax.a + gradenMin.a) / 2; //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.a--;
       Serial.print("Graden A = ");
       Serial.println(graden.a);
       elbowACalc(graden.a); //Opnieuw coördinaten van elleboog A berekenen
       distance = distanceCalc(polsAX, polsAY, elbowAX, elbowAY, polsZ, elbowAZ);  //Opnieuw de afstand tussen pols A en elleboog A berekenen
       Serial.print("Distance A = ");
       Serial.println(distance);
-      delay(1000);
     }else if(L_ARM_2 - distance > resolution){  //De berekende afstand is kleiner dan de arm
-      graden.a = (180 - graden.a)/2 + graden.a; //Graden kiezen in het midden tussen 180 en de vorige waarde
+      /*gradenMin.a = graden.a; //Ondergrens van aantal graden aanpassen naar momentele positie
+      graden.a = (gradenMax.a + gradenMin.a) / 2;  //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.a++;
       elbowACalc(graden.a); //Opnieuw coördinaten van elleboog A berekenen
       Serial.print("Graden A = ");
       Serial.println(graden.a);
       distance = distanceCalc(polsAX, polsAY, elbowAX, elbowAY, polsZ, elbowAZ);  //Opnieuw de afstand tussen pols A en elleboog A berekenen
       Serial.print("Distance A = ");
       Serial.println(distance);
-      delay(1000);
     }
   }
   Serial.print("Graden A = ");  //Waarde voor servo A printen als de juiste waarde is gevonden
   Serial.println(graden.a);
-  delay(1000);
+  delay(100);
   elbowBCalc(graden.b);
   distance = distanceCalc(polsBX, polsBY, elbowBX, elbowBY, polsZ, elbowBZ);
   Serial.println(distance);
 
   while (L_ARM_2 - distance < -resolution || L_ARM_2 - distance > resolution){
     if(L_ARM_2 - distance < -resolution){
-      graden.b = graden.b / 2;
+      /*gradenMin.b =  graden.b; //Bovengrens van aantal graden aanpassen naar momentele positie
+      graden.b = (gradenMax.b + gradenMin.b) / 2; //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.b--;
       Serial.print("Graden B = ");
       Serial.println(graden.b);
-      elbowACalc(graden.b);
+      elbowBCalc(graden.b);
       distance = distanceCalc(polsBX, polsBY, elbowBX, elbowBY, polsZ, elbowBZ);
       Serial.print("Distance B = ");
       Serial.println(distance);
-      delay(1000);
     }else if(L_ARM_2 - distance > resolution){
-      graden.b = (180 - graden.b)/2 + graden.b;
-      elbowACalc(graden.b);
+      /*gradenMax.b = graden.b; //Ondergrens van aantal graden aanpassen naar momentele positie
+      graden.b = (gradenMax.b + gradenMin.b) / 2;  //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.b++;
+      elbowBCalc(graden.b);
       Serial.print("Graden B = ");
       Serial.println(graden.b);
       distance = distanceCalc(polsBX, polsBY, elbowBX, elbowBY, polsZ, elbowBZ);
       Serial.print("Distance B = ");
       Serial.println(distance);
-      delay(1000);
     }
   }
   Serial.print("Graden B = ");
   Serial.println(graden.b);
-  delay(1000);
+  delay(100);
   elbowCCalc(graden.c);
   distance = distanceCalc(polsCX, polsCY, elbowCX, elbowCY, polsZ, elbowCZ);
   Serial.println(distance);
 
   while (L_ARM_2 - distance < -resolution || L_ARM_2 - distance > resolution){
     if(L_ARM_2 - distance < -resolution){
-      graden.c = graden.c / 2;
+      /*gradenMax.c =  graden.c; //Bovengrens van aantal graden aanpassen naar momentele positie
+      graden.c = (gradenMax.c + gradenMin.c) / 2; //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.c--;
       Serial.print("Graden C = ");
       Serial.println(graden.c);
-      elbowACalc(graden.c);
+      elbowCCalc(graden.c);
       distance = distanceCalc(polsCX, polsCY, elbowCX, elbowCY, polsZ, elbowCZ);
       Serial.print("Distance C = ");
       Serial.println(distance);
-      delay(1000);
     }else if(L_ARM_2 - distance > resolution){
-      graden.c = (180 - graden.c)/2 + graden.c;
-      elbowACalc(graden.c);
+      /*gradenMin.c = graden.c; //Ondergrens van aantal graden aanpassen naar momentele positie
+      graden.c = (gradenMax.c + gradenMin.c) / 2;  //Waarde kiezen in het midden tussen de maximale en minimale waarde*/
+      graden.c++;
+      elbowCCalc(graden.c);
       Serial.print("Graden C = ");
       Serial.println(graden.c);
       distance = distanceCalc(polsCX, polsCY, elbowCX, elbowCY, polsZ, elbowCZ);
       Serial.print("Distance C = ");
       Serial.println(distance);
-      delay(1000);
     }
   }
 }
@@ -228,36 +201,22 @@ void move(int x, int y, int z){
   Serial.println(z);
   degreeCalc(x, y, z);
 
-  /*if(graden.a < 0){
-    graden.a = graden.a * -1;
-  }
-  if(graden.b < 0){
-    graden.b = graden.b * -1;
-  }
-  if(graden.c < 0){
-    graden.c = graden.c * -1;
-  }
-
-  graden.a = 180 - graden.a;
-  graden.b = 180 - graden.b;
-  graden.c = 180 - graden.c;
-
-  if(graden.a < 0){
-    graden.a = graden.a * -1;
-  }
-  if(graden.b < 0){
-    graden.b = graden.b * -1;
-  }
-  if(graden.c < 0){
-    graden.c = graden.c * -1;
-  }*/
-
   Serial.print("Graden A = ");
   Serial.println(graden.a);
   Serial.print("Graden B = ");
   Serial.println(graden.b);
   Serial.print("Graden C = ");
   Serial.println(graden.c);
+/*
+  if(graden.a > 180){
+    graden.a = graden.a - 180;
+  }
+  if(graden.b > 180){
+    graden.b = graden.b - 180;
+  }
+  if(graden.c > 180){
+    graden.c = graden.c - 180;
+  }*/
 
   if (graden.a >= MIN && graden.a <= MAX){
     servoA.write(graden.a);
@@ -279,9 +238,31 @@ void debug(){
   servoA.write(graden.a);
   servoB.write(graden.b);
   servoC.write(graden.c);
-  elbowACalc(45); //Coördinaten elleboog A berekenen
-  elbowBCalc(45); //Coördinaten elleboog B berekenen
-  elbowCCalc(45); //Coördinaten elleboog C berekenen
+  elbowACalc(180); //Coördinaten elleboog A berekenen
+  elbowBCalc(180); //Coördinaten elleboog B berekenen
+  elbowCCalc(180); //Coördinaten elleboog C berekenen
+  polsCalc(0, 0, 100);
+  float distanceA = distanceCalc(polsAX, polsAY, elbowAX, elbowAY, polsZ, elbowAZ);
+  float distanceB = distanceCalc(polsBX, polsBY, elbowBX, elbowBY, polsZ, elbowBZ);
+  float distanceC = distanceCalc(polsCX, polsCY, elbowCX, elbowCY, polsZ, elbowCZ);
+  Serial.print("PolsAX: ");
+  Serial.println(polsAX);
+  Serial.print("PolsAY: ");
+  Serial.println(polsAY);
+  Serial.println();
+  Serial.print("PolsBX: ");
+  Serial.println(polsBX);
+  Serial.print("PolsBY: ");
+  Serial.println(polsBY);
+  Serial.println();
+  Serial.print("PolsCX: ");
+  Serial.println(polsCX);
+  Serial.print("PolsCY: ");
+  Serial.println(polsCY);
+  Serial.println();
+  Serial.print("PolsZ: ");
+  Serial.println(polsZ);
+  Serial.println();
   Serial.print("elbowAX: ");
   Serial.println(elbowAX);
   Serial.print("elbowAY: ");
@@ -303,4 +284,10 @@ void debug(){
   Serial.print("elbowCZ: ");
   Serial.println(elbowCZ);
   Serial.println();
+  Serial.print("Distance A: ");
+  Serial.println(distanceA);
+  Serial.print("Distance B: ");
+  Serial.println(distanceB);
+  Serial.print("Distance C: ");
+  Serial.println(distanceC);
 }
--- Delta bot/src/mydef.h
+++ Delta bot/src/mydef.h
@@ -8,14 +8,54 @@
 #define SERVO_B_Y 95
 #define SERVO_C_X 55
 #define SERVO_C_Y -95
-#define SERVO_Z 265
+#define SERVO_Z 29
+#define Z_BASIS 294
 #define L_ARM_1 45
 #define L_ARM_2 218
 #define L_TOP 32
 #define L_BASE 100
-#define Z_TOOL_OFFSET 10
-#define MIN 85
-#define MAX 180
-#define resolution 1
+#define Z_TOOL_OFFSET 22
+#define MIN 0
+#define MAX 360
+#define resolution 10
 #define rad60 1.0471975
 #define radOmreken 0.0174532
+#define testGraden 25
+
+float polsAX = 0;
+float polsAY = 0;
+float polsBX = 0;
+float polsBY = 0;
+float polsCX = 0;
+float polsCY = 0;
+float polsZ = 0;
+
+float elbowAX = 0;
+float elbowAY = 0;
+float elbowAZ = 0;
+float elbowBX = 0;
+float elbowBY = 0;
+float elbowBZ = 0;
+float elbowCX = 0;
+float elbowCY = 0;
+float elbowCZ = 0;
+
+//Struct om de graden voor de servo's op te slaan
+struct servos{
+  int a;
+  int b;
+  int c;
+};
+
+servos graden;
+servos gradenMax; //Hoogste aantal graden van de laatste waardes
+servos gradenMin; //Laagste aantal graden van de laatste waardes
+
+//Struct om de naar radialen omgerekende graden op te slaan
+struct Radialen{
+  float a;
+  float b;
+  float c;
+};
+
+Radialen radialen;
